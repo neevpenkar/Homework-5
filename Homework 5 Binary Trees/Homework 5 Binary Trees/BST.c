8728683a60aa5c4cdb65f8c816288209977aef05
neevpenkar <neev.penkar@gmail.com>
2022-05-23 14:17:53 +0300

    1

---
+++ Homework 5 Binary Trees/Homework 5 Binary Trees/BST.c
@@ -0,0 +1,20 @@
+#include "BST.h"
+#include <stdlib.h>
+#include "TreePrintLibrary.h"
+
+void initBST(BST* bst) {
+	bst->root = NULL;
+}
+voi/*d insertBST(BST* bst, int value) {
+	TreeNode* m = bst->root;
+	if (m == NULL) {
+		m = (TreeNode*)malloc(sizeof(TreeNode));
+		m->left = NULL; m->right = NULL;
+		m->element = value;
+	}
+	else if (m != NULL) {
+		if (value > m->element) {
+			insertBST(bst->)
+		}
+	}*/
+}
---
+++ Homework 5 Binary Trees/Homework 5 Binary Trees/MAIN.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "BST.h"
+#include "TreePrintLibrary.h"
+
+int main() {
+	BST tree;
+	initBST(&tree);
+}
